53cda577c83cc085570cbf938a9b5b7d73a59995
Roman Shashin <propeller1992@gmail.com>
2023-03-18 22:13:16 +0300

    перавя глава учебника

---
+++ main.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+'use strict';
+
+paper.install(window);
+paper.setup(document.getElementByid('mainCanvas'));
+
+var с = Shape.Circle(200, 200, 50);
+c.fillColor = 'green';
+
+paper.view.draw();
+
+};
